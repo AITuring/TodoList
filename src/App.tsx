9f1cefd05b7d2c668bfbd2ad92f1ea9fcdac8771
lishengyu <lishengyu@baidu.com>
2021-07-07 09:38:26 +0800

    还是跨域问题,还没解决...

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,10 @@ interface IProps {
 }
 
 const App: FC<IProps>= (): ReactElement=> {
-    fetch('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN', {
+    fetch('/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN', {
         headers: {
             "Access-Control-Allow-Origin": "*",
         },
-        mode: "no-cors"
     }).then(response =>{
     console.log(response)
     })
--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -3,13 +3,13 @@ const createProxyMiddleware = require('http-proxy-middleware');
 
 module.exports = function (app: { use: (arg0: string, arg1: any) => void; }) {
     app.use(
-        '/',
+        '/HPImageArchive.aspx',
         createProxyMiddleware({
             target: 'https://cn.bing.com/',
             changeOrigin: true,
-            pathRewrite: {
-                '^/': '/'
-            }
+            // pathRewrite: {
+            //     '^/HPImageArchive.aspx': '/'
+            // }
         })
     );
 };

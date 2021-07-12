3fdc6c891526a94ba9037c1687b054bf29b71ccf
AITuring <shspku365@gmail.com>
2021-07-13 00:16:58 +0800

    图标重构

--- src/components/TodoList/List/item.tsx
+++ src/components/TodoList/List/item.tsx
@@ -1,6 +1,9 @@
 import React, { FC, ReactElement } from "react";
 import { ITodo } from "../typings";
 import deleteImg from "assets/delete.png";
+import todoImg from "assets/todo.png";
+import doneImg from "assets/done.png";
+import doneImg2 from "assets/done2.png";
 
 interface IProps {
 	todo: ITodo;
@@ -23,28 +26,33 @@ const TdItem: FC<IProps> = ({
 		>
 			{
 				completed ?
+					<img
+						src={doneImg}
+						className="w-7 block mr-2 cursor-pointer"
+						onClick={() => toggleTodo(id)}
+					/>
+					:
+					<img
+						src={todoImg}
+						className="w-7 block mr-2 cursor-pointer"
+						onClick={() => toggleTodo(id)}
+					/>
+			}
+			{
+				completed ?
 					<div
-						className="bg-gray-300 w-8 h-7 rounded-full border-2 border-green-500 flex justify-center items-center m-2"
-						onClick={() => toggleTodo(id)}
+						className="w-full ml-4 font-bold text-gray-600 dark:text-gray-400"
+						style={{ textDecoration: 'line-through #43CD80 0.2rem'}}
 					>
-						<div
-							className="w-3 h-3 bg-green-500 rounded-full"
-							onClick={() => toggleTodo(id)}
-						></div>
+						{content}
 					</div>
-					: <div
-						// 这里的○不圆，有问题
-						className="bg-gray-300 w-8 h-7 rounded-full m-2"
-						onClick={() => toggleTodo(id)}
+					:
+					<div
+						className="w-full ml-4 font-bold text-gray-600 dark:text-gray-200"
 					>
+						{content}
 					</div>
 			}
-			<div
-				className="w-full ml-4 font-bold text-gray-600 dark:text-gray-200"
-				style={{ textDecoration: completed ? 'line-through' : 'none' }}
-			>
-				{content}
-			</div>
 			<img
 				src={deleteImg}
 				className="w-7 block mr-2 cursor-pointer"
